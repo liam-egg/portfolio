d5a3e4454c2dc24afdd118341a41d5c0059250c2
liam-egg <elseifbusiness@gmail.com>
2026-09-14 00:26:47 -0500

    Update misc functions

--- api/request-post.js
+++ api/request-post.js
@@ -9,7 +9,7 @@
 //
 // The destination address is hardcoded below since it isn't a secret.
 
-const TO_EMAIL = 'liameggs@ucla.edu';
+const TO_EMAIL = 'liameggs+blog@ucla.edu';
 const MAX_LEN = 2000;
 
 module.exports = async function handler(req, res) {
